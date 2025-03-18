8cb6fc9866f0d88e12140c54121c3117d5b3dba9
Your Name <you@example.com>
2025-03-18 01:05:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿namespace ConvertingRomans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            static int NumberOfRomans()
+            {
+
+                int input = Int32.Parse(Console.ReadLine());
+                return input;
+            }
+
+            static string[] ReadingRomans(int numberOfLines)
+            {
+
+                string[] romanValues = new string[numberOfLines];
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    romanValues[i] = Console.ReadLine();
+
+                }
+
+                return romanValues;
+            }
+
+            static string[] RomanNumbersToArabic(string[] romanNumbers)
+            {
+                string[] arabicNumbers = new string[romanNumbers.Length];
+                for (int i = 0;i < romanNumbers.Length;i++)
+                {
+                    arabicNumbers[i] = RomanNumberToArabic(romanNumbers[i]);
+                }
+                return arabicNumbers;
+
+            }
+
+            //static void ConvertingRomans(string[] romanValues) { }
+
+            static string RomanNumberToArabic(string romanNumber)
+            {
+                Dictionary<char, int> RomanArabicDict = new Dictionary<char, int>();
+                RomanArabicDict.Add('I', 1);
+                RomanArabicDict.Add('V', 5);
+                RomanArabicDict.Add('X', 10);
+                RomanArabicDict.Add('L', 50);
+                RomanArabicDict.Add('C', 100);
+                RomanArabicDict.Add('D', 500);
+                RomanArabicDict.Add('M', 1000);
+                int arabicNumber = 0;
+
+                for (int i = 0; i < romanNumber.Length; i++)
+                {
+                    /*if ((i == romanNumber.Length - 1) || (RomanArabicDict[romanNumber[i].ToString()] >= RomanArabicDict[romanNumber[i + 1].ToString()]))
+                    {
+                        arabicNumber += RomanArabicDict[romanNumber[i].ToString()];
+                    }
+                    else
+                    {
+                        arabicNumber -= RomanArabicDict[romanNumber[i].ToString()];
+                    }*/
+                    int valueToSubtract = RomanArabicDict[romanNumber[i]];
+                    int valueToAdd = RomanArabicDict[romanNumber[i]];
+                    int moveiTo = i;
+                    if (i != romanNumber.Length - 1)
+                    {
+                        bool repeatingNumber = true;
+                        bool arabicNumberUpdated = false;
+                        for (int j = i + 1; j < romanNumber.Length; j++)
+                        {
+
+                            if (RomanArabicDict[romanNumber[i]] != RomanArabicDict[romanNumber[j]]) repeatingNumber = false;
+                            if (repeatingNumber)
+                            {
+                                valueToAdd += RomanArabicDict[romanNumber[j]]; 
+                                moveiTo = j;
+                            }
+                            
+                            if (RomanArabicDict[romanNumber[i]] < RomanArabicDict[romanNumber[j]])
+                            {
+
+                                arabicNumber -= valueToSubtract;
+                                arabicNumberUpdated = true;
+                                moveiTo = j-1;
+                                break;
+                            }
+
+                            valueToSubtract += RomanArabicDict[romanNumber[j]];
+
+
+
+                        }
+                        if (arabicNumberUpdated) {
+                            i = moveiTo;
+                            continue; }
+                        else {
+                            arabicNumber += valueToAdd;
+                            i = moveiTo;
+                            continue;
+                        }
+                            
+
+                    }
+                    arabicNumber += valueToAdd;
+
+
+                }
+                return arabicNumber.ToString();
+            }
+
+            static void WritingArabic(string[] arabicNumbers)
+            {
+                foreach (string arabicNumber in arabicNumbers)
+                {
+                    Console.WriteLine(arabicNumber);
+                }
+            }
+
+            static void ConvertingRomans()
+            {
+                WritingArabic(RomanNumbersToArabic(ReadingRomans(NumberOfRomans())));
+            }
+
+            ConvertingRomans();
+        }
+
+
+        //Console.WriteLine(ReadingRomans(NumberOfRomans()));
+
+    }
+}
